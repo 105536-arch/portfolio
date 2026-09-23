e6ef57b1a41098e3c12e43e02f354012db1baecb
林奕辰 <yl6834145@gmail.com>
2026-09-23 15:51:47 +0800

    py

--- 1.py
+++ 1.py
@@ -1,1 +1,11 @@
-print("hello")
+scores =[78, 92, 65, 88, 73, 95, 81, 69, 84, 90]
+highest = max(scores)
+lowest = min(scores)
+average = sum(scores) / len(scores) 
+top3 = sorted(scores, reverse=True) [:3]
+print("=== 成績分析 ===") 
+print(" :", len(scores)) 
+print(" :", highest) 
+print(" :", lowest) 
+print (" :", average) 
+print("前三名:", top3)
